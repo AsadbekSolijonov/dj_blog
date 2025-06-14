fff4471183cb322b793f8b242987a5427b246116
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-06-14 18:12:16 +0500

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,35 @@
+import random
+from dataclasses import dataclass
+
+from django.shortcuts import render
+
+from blog.models import Blog
+
+
+@dataclass
+class Students:
+    first_name: str
+    last_name: str
+    phone: str
+    payed: bool
+
+
+st1 = Students('Alisher', 'Eshmatov', '998 91 903 34-43', True)
+st2 = Students('Alex', 'Gorden', '998 91 903 34-43', False)
+st3 = Students('Avazbek', 'Boymuradov', '998 90 903 00-42', True)
+st4 = Students('Abror', 'Sotiboldiyev', '998 33 300 40-43', True)
+students = [st1, st2, st3, st4]
+
+
+def home(request):
+    a = random.randint(1, 100)
+    b = random.randint(101, 1000)
+    c = a + b
+
+    context = {
+        "key1": a,
+        "key2": b,
+        "key3": c,
+        "blogs": Blog.objects.all()
+    }
+    return render(request, template_name='blog/base.html', context=context)
