10eb5880b0b3d816422686b3b08a7753a9977612
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-06-19 16:50:16 +0500

    add image field

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # 2025-06-19 16:28
+    updated_at = models.DateTimeField(auto_now=True)  # 2025-06-22 15:28
 
     def __str__(self):
         return f"{self.title}"
-
