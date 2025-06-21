cb11c20caab63fadb114f6f5ae3cd4e865c4ddf1
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-06-21 18:03:05 +0500

    type blogs added

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from blog.models import Blog
 class BlogForms(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ['title', 'content', 'image', 'is_active']
+        fields = ['title', 'content', 'type', 'image', 'is_active']
--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 
 class Blog(models.Model):
+    TYPE = {
+        'Jounal': 'Journal',
+        "Life updates": 'Life updates',
+        'Travel stories': 'Travel stories',
+        'Personal growth': 'Personal growth'
+    }
     title = models.CharField(max_length=200, verbose_name='Sarlavha')
     content = models.TextField(verbose_name='Tavsif')
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
     is_active = models.BooleanField(default=True)
+    type = models.CharField(max_length=50, choices=TYPE, default='Jounal')
     created_at = models.DateTimeField(auto_now_add=True)  # 2025-06-19 16:28
     updated_at = models.DateTimeField(auto_now=True)  # 2025-06-22 15:28
 
