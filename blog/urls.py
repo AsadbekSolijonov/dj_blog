89e9867bd0a6d4926467ed14f3e9548aa052b38d
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-06-24 15:51:04 +0500

    blog delete

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,9 @@ from blog import views as blog_views
 
 urlpatterns = [
     path('', blog_views.home, name='home'),
-    path('in_actives', blog_views.in_active, name='in_active_blogs'),
-    path('<int:blog_id>/view', blog_views.detail, name='detail'),
     path('about/', blog_views.about, name='about'),
+    path('in_actives/', blog_views.in_active, name='in_active_blogs'),
+    path('<int:blog_id>/view', blog_views.detail, name='detail'),
     path('<int:blog_id>/update', blog_views.update, name='update'),
+    path('<int:blog_id>/delete', blog_views.delete, name='delete'),
 ]
